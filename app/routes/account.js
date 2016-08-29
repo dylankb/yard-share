02f392122893374cf7caac19e458dd1b76f5ab98
Dylan Barnard and Molly McGlone <dylan+molly@>
2016-08-29 14:25:52 -0700

    Add basic account creation

--- app/routes/account.js
+++ app/routes/account.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    createAccount(params) {
+      if (params.userType === "renter") {
+        console.log(params.userType);
+        var newRenter = this.store.createRecord('renter', params);
+        newRenter.save();
+      } else if (params.userType === "owner") {
+        console.log(params.userType);
+        var newOwner = this.store.createRecord('owner', params);
+        console.log(newOwner, "this is an owner");
+        newOwner.save();
+      }
+      this.transitionTo('index');
+    }
+  }
 });

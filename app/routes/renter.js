b61865491caef1631209e2912a04f903fdf8b23c
Dylan Barnard and Molly McGlone <dylan+molly@>
2016-08-30 09:48:02 -0700

    Create functional renter-details and renter-update components

--- app/routes/renter.js
+++ app/routes/renter.js
@@ -4,5 +4,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('renter', params.renter_id);
+  },
+
+  actions: {
+    updateRenter(params) {
+      var renter = params.renter;
+      Object.keys(params).forEach(function(key) {
+        if (params[key]!==undefined) {
+          renter.set(key, params[key]);
+        }
+      });
+      renter.save();
+    }
   }
 });

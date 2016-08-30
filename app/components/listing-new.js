7410c9f02a338330de8ae37d2062ac9f794042d6
Brett New and Julia Dickey <brett+julia@>
2016-08-30 10:04:12 -0700

    get checkboxes to work on new-listing.hbs

--- app/components/listing-new.js
+++ app/components/listing-new.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  camping: false,
+  actions: {
+    addAmmenity(){
+      this.set(event.target.id, event.target.checked);
+    },
+    saveListing() {
+      var params = {
+        water: this.get('water'),
+        pets: this.get('pets'),
+        bathroom: this.get('bathroom'),
+        parking: this.get('parking'),
+        alcohol: this.get('alcohol'),
+        smoking: this.get('smoking'),
+        wifi: this.get('wifi'),
+        fires: this.get('fires'),
+        rvHookup: this.get('rvHookup'),
+        electricity: this.get('electricity'),
+        grill: this.get('grill')
+      };
+debugger;
+    }
+  }
 });
--- app/models/listing.js
+++ app/models/listing.js
@@ -14,6 +14,7 @@ export default DS.Model.extend({
   camping: DS.attr(),
   gardening: DS.attr(),
   events: DS.attr(),
+
   water: DS.attr(),
   pets: DS.attr(),
   bathroom: DS.attr(),

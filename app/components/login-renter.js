4c0553d04e7baa86c0fd09d51dabcbdf2987e3ba
Dylan Barnard and Molly McGlone <dylan+molly@>
2016-08-30 09:17:53 -0700

    Create seperate log in forms for each user type

--- app/components/login-renter.js
+++ app/components/login-renter.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  loginRenterForm: false,
   actions: {
+    loginRenterFormShow() {
+      this.set('loginRenterForm', true);
+    },
     renterLogin() {
       var params = {
         username: this.get('username') || "",
         password: this.get('password') || ""
       };
+      this.set('loginRenterForm', false);
       this.sendAction('renterLogin', params);
     }
   }

cc671d3a4b872babe7c6c36054e73f6a6ae21c18
Brett New and Molly McGlone <brett+molly@>
2016-08-31 16:26:30 -0700

    add functionality to make google map automatically display when page loads.

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -1,29 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showMap: false,
   map: Ember.inject.service('google-map'),
-
-  actions: {
-    showMap(listing) {
-      var container = this.$('.map-display')[0];
-      var options = {
-        center: this.get('map').center(listing.get('latitude'), listing.get('longitude')),
-        zoom: 15
-      };
-      var newMap = this.get('map').findMap(container, options);
-      var markerOptions = {
-        position: {lat: listing.get('latitude'), lng: listing.get('longitude')},
-        map: newMap,
-        icon: '/images/leaf_icon.png'
-      };
-      var content = {
-        content: "<h4>" + listing.get('title') + "</h4>"
-      };
-      var infowindow = this.get('map').createInfoWindow(content);
-      var marker = this.get('map').createMarker(markerOptions);
-      marker.addListener('click', function() {
-        infowindow.open(newMap, marker);
-      });
-    }
+  didInsertElement() {
+    var container = this.$('.map-display')[0];
+    var options = {
+      center: this.get('map').center(this.get('listing.latitude'), this.get('listing.longitude')),
+      zoom: 15
+    };
+    var newMap = this.get('map').findMap(container, options);
+    var markerOptions = {
+      position: {lat: this.get('listing.latitude'), lng: this.get('listing.longitude')},
+      map: newMap,
+      icon: '/images/leaf_icon.png'
+    };
+    var content = {
+      content: "<h4>" + this.get('listing.title') + "</h4>"
+    };
+    var infowindow = this.get('map').createInfoWindow(content);
+    var marker = this.get('map').createMarker(markerOptions);
+    marker.addListener('click', function() {
+      infowindow.open(newMap, marker);
+    });
   }
 });

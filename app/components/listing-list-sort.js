7a3d8c244ff86ba2e0a1eb59ab7fc7656ecb75cc
Dylan Barnard and Julia Dickey <dylan+julia@>
2016-08-31 16:02:37 -0700

    User can filter by multiple uses and neighborhood

--- app/components/listing-list-sort.js
+++ app/components/listing-list-sort.js
@@ -1,44 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  showGardening: false,
-  showCamping: false,
-  showEvents: false,
-  showNeighborhood: false,
+  neighborhoods: ["All", "Downtown", "Pearl District", "Old Chinatown", "NW Portland/Nob Hill", "Lloyd District", "Mississippi/Williams", "Alberta Arts District", "Division/Clinton", "Belmont", "Hawthorne", "Sellwood-Moreland"],
+  showGardening: true,
+  showCamping: true,
+  showEvents: true,
+  showNeighborhood: "All",
   listingsByUse: Ember.computed('listings', 'showGardening', 'showCamping', 'showEvents', 'showNeighborhood', function() {
     var self = this;
 
-    // var findNeighborhood = function() {
-    //   if (self.get('showNeighborhood') === false) {
-    //     return true;
-    // } else {
-    //   return listing.get('neighborhood') === self.get('showNeighborhood');
-    // }
+    var findNeighborhood = function(listing) {
+      if (self.get('showNeighborhood') === "All") {
+        return true;
+      } else {
+        return listing.get('neighborhood') === self.get('showNeighborhood');
+      }
+    };
 
     return this.get('listings').filter(function(listing) {
       if (listing.get('gardening') === true && self.get('showGardening') === true) {
-
-        // findNeighborhood();
-
-        if (self.get('showNeighborhood') === false) {
-          return true;
-        } else {
-          return listing.get('neighborhood') === self.get('showNeighborhood');
-        }
+        return findNeighborhood(listing);
       }
       else if (listing.get('camping') === true && self.get('showCamping') === true) {
-        if (self.get('showNeighborhood') === false) {
-          return true;
-        } else {
-          return listing.get('neighborhood') === self.get('showNeighborhood');
-        }
+        return findNeighborhood(listing);
       }
       else if (listing.get('events') === true && self.get('showEvents') === true) {
-        if (self.get('showNeighborhood') === false) {
-          return true;
-        } else {
-          return listing.get('neighborhood') === self.get('showNeighborhood');
-        }
+        return findNeighborhood(listing);
+      } else if (self.get('showNeighborhood') === "All") {
+        return listing.get('neighborhood') === self.get('showNeighborhood');
       }
     });
   }),
@@ -55,17 +44,6 @@ export default Ember.Component.extend({
     eventsUse() {
       this.set(event.target.id, event.target.checked);
       this.set('showEvents', event.target.checked);
-    },
-    chooseNeighborhood() {
-      this.set(event.target.id, event.target.checked);
-      // debugger;
-      if (event.target.checked === true){
-        this.set('showNeighborhood', event.target.id);
-        console.log("true", this.get('showNeighborhood'));
-      } else if (event.target.checked === false){
-        this.set('showNeighborhood', false);
-        console.log("false", this.get('showNeighborhood'));
-      }
     }
   }
 });

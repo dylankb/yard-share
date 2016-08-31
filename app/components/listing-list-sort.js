447f8139f31efb98a189838f8a23ba0b796243a7
Dylan Barnard and Julia Dickey <dylan+julia@>
2016-08-31 14:52:49 -0700

    Users can filter by a neighborhood

--- app/components/listing-list-sort.js
+++ app/components/listing-list-sort.js
@@ -4,17 +4,41 @@ export default Ember.Component.extend({
   showGardening: false,
   showCamping: false,
   showEvents: false,
-  listingsByUse: Ember.computed('listings', 'showGardening', 'showCamping', 'showEvents', function() {
+  showNeighborhood: false,
+  listingsByUse: Ember.computed('listings', 'showGardening', 'showCamping', 'showEvents', 'showNeighborhood', function() {
     var self = this;
+
+    // var findNeighborhood = function() {
+    //   if (self.get('showNeighborhood') === false) {
+    //     return true;
+    // } else {
+    //   return listing.get('neighborhood') === self.get('showNeighborhood');
+    // }
+
     return this.get('listings').filter(function(listing) {
       if (listing.get('gardening') === true && self.get('showGardening') === true) {
-        return true;
+
+        // findNeighborhood();
+
+        if (self.get('showNeighborhood') === false) {
+          return true;
+        } else {
+          return listing.get('neighborhood') === self.get('showNeighborhood');
+        }
       }
       else if (listing.get('camping') === true && self.get('showCamping') === true) {
-        return true;
+        if (self.get('showNeighborhood') === false) {
+          return true;
+        } else {
+          return listing.get('neighborhood') === self.get('showNeighborhood');
+        }
       }
       else if (listing.get('events') === true && self.get('showEvents') === true) {
-        return true;
+        if (self.get('showNeighborhood') === false) {
+          return true;
+        } else {
+          return listing.get('neighborhood') === self.get('showNeighborhood');
+        }
       }
     });
   }),
@@ -31,6 +55,17 @@ export default Ember.Component.extend({
     eventsUse() {
       this.set(event.target.id, event.target.checked);
       this.set('showEvents', event.target.checked);
+    },
+    chooseNeighborhood() {
+      this.set(event.target.id, event.target.checked);
+      // debugger;
+      if (event.target.checked === true){
+        this.set('showNeighborhood', event.target.id);
+        console.log("true", this.get('showNeighborhood'));
+      } else if (event.target.checked === false){
+        this.set('showNeighborhood', false);
+        console.log("false", this.get('showNeighborhood'));
+      }
     }
   }
 });
